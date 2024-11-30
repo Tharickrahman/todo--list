8f5653a99bca67de3f92c46706b5fe164522a67c
Tharick Rahman <tharickrahman93@gmail.com>
2024-11-30 14:06:25 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='foot'>
+    <a href="https://www.linkedin.com/in/tharick-rahman-a-5b640b227" target='blanck'>Linkedin</a>
+    <a href="https://github.com/Tharickrahman" target='_blank'>Git Hub</a>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+
+        <div className='head'>
+            <h1>Todo-List</h1>
+            <h2>By Tharick Rahman</h2>
+        </div>
+    )
+}
+
